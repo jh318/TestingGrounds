5d231ea78dd84f9596fa02901346150b8bde7c92
John <jhawley318@gmail.com>
2018-09-15 22:52:05 -0500

    made infiniteterrain gamemode that pools navmeshes. had to refactor some inheritance that was causing the editor to crash regarding gamemode parents

---
+++ Source/TestingGrounds/InfiniteTerrainGameMode.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFrameWork/GameMode.h"
+#include "InfiniteTerrainGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGGROUNDS_API AInfiniteTerrainGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Bounds Pool")
+	void PopulateBoundsVolumePool();
+
+private:
+	void AddToPool(class ANavMeshBoundsVolume *VolumeToAdd);
+	
+	
+};
